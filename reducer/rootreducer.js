6a5ae657abd40bbaf544f54ef95915685131e2c3
anandhiyer <anandhariharaniyer@gmail.com>
2017-08-19 09:04:18 -0400

    redux form basic validation

--- reducer/rootreducer.js
+++ reducer/rootreducer.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux'
 import { reducer as formReducer } from 'redux-form'
-import todoReducer from './todoReducer';
 
 const rootReducer = combineReducers({
   // ... your other reducers here ...
-  todos: todoReducer,
   form: formReducer     // <---- Mounted at 'form'
 });
 
